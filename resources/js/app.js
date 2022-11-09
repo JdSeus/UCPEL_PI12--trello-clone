a8cf8143798b71c30372752e0b8effcd4b11d939
Juliano Seus <julianoseusdev@outlook.com>
2022-11-09 20:57:31 -0300

    Installation of Tailwind CSS

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
-require('./bootstrap');
+import Main from './Main.js';
+
+Main.main();
+
+window._ = require('lodash');
+
+try {
+    require('bootstrap');
+} catch (e) {}
+
+
+window.axios = require('axios');
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
