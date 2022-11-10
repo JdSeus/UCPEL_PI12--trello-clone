f7e514333e430e52f1646d29bb4d16a9756f11e6
Juliano Seus <julianoseusdev@outlook.com>
2022-11-09 21:20:42 -0300

    Preparation of Home

--- routes/web.php
+++ routes/web.php
@@ -13,15 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\HomeController;
 
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Auth::routes();
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
 });
 
-Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
