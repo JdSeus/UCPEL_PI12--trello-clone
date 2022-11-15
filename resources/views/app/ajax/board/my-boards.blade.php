dba4f231c2b58983e76be6903878159c69c980a3
Juliano Seus <julianoseusdev@outlook.com>
2022-11-15 14:13:02 -0300

    Editing Board

--- resources/views/app/ajax/board/my-boards.blade.php
+++ resources/views/app/ajax/board/my-boards.blade.php
@@ -8,9 +8,14 @@
         @if (count($boards) > 0)
             @foreach($boards as $board)
                 <div class="mb-4">
-                    <span class="block text-gray-700 text-sm font-bold mb-2">
-                        {{ $board->title }}
-                    </span>
+                    <div class="p-[5px] flex items-baseline justify-between border-b border-t border-solid border-black">
+                        <div class="block text-gray-700 text-sm font-bold mb-2">
+                            {{ $board->title }}
+                        </div>
+                        <a class="block cursor-pointer text-white w-fit p-[5px] bg-yellow-600" hx-get="{{ route('ajax.board.edit', $board->id) }}">
+                            Editar
+                        </a>
+                    </div>
                 </div>
             @endforeach
         @else 
@@ -21,7 +26,7 @@
             </div>
         @endif
 
-        <div class="mx-auto block cursor-pointer text-white w-fit p-[5px] bg-yellow-600" hx-get="{{ route('ajax.board.create') }}" hx-target="#js-dialog">
+        <div class="mx-auto block cursor-pointer text-white w-fit p-[5px] bg-green-600" hx-get="{{ route('ajax.board.create') }}" hx-target="#js-dialog">
             Criar Novo Quadro
         </div>
 
