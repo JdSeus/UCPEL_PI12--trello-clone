e562fe8122dafff1a4ae6465029c7cbd2ac66e6e
Juliano Seus <julianoseusdev@outlook.com>
2022-11-17 21:16:52 -0300

    Adjust on Columns

--- resources/views/app/board/content.blade.php
+++ resources/views/app/board/content.blade.php
@@ -1,4 +1,4 @@
-<section class="min-h-[100vh] flex @if(isset($board->columns) && count($board->columns)) items-start @else items-center @endif bg-gray-400 pt-[calc(108px+50px)]">
+<section class="min-h-[100vh] max-w-[100vw] overflow-x-scroll flex @if(isset($board->columns) && count($board->columns)) items-start @else items-center @endif bg-gray-400 pt-[calc(108px+50px)]">
 
     <div class="absolute mx-auto w-fit top-[20px] left-[20px]">
         <div class="min-w-[300px] bg-white rounded p-[20px]">
@@ -13,7 +13,7 @@
     @if(isset($board->columns) && count($board->columns))
         @foreach($board->columns as $column)
             <div class="ml-[20px] mr-[20px] w-fit">
-                <div class="min-w-[300px] bg-white rounded p-[20px]">
+                <div class="min-w-[max(250px,calc(100vw-100px))] sm:min-w-[300px] bg-white rounded p-[20px]">
                     <p class="text-xl font-bold text-gray-700 text-center">
                         {{ $column->title }}
                     </p>
@@ -21,7 +21,7 @@
             </div>
         @endforeach
         <div class="ml-[20px] mr-[20px] w-fit">
-            <div class="min-w-[300px] bg-white rounded p-[20px]">
+            <div class="min-w-[max(250px,calc(100vw-100px))] sm:min-w-[300px] bg-white rounded p-[20px]">
                     
                 <div class="mx-auto block cursor-pointer text-white w-fit p-[5px] transition duration-300 bg-green-500 hover:bg-green-700" hx-get="{{ route('ajax.column.create', ['board_id' => $board->id]) }}" hx-target="#js-dialog">
                     Criar Coluna
@@ -31,7 +31,7 @@
         </div>
     @else
         <div class="mx-auto w-fit">
-            <div class="min-w-[300px] bg-white rounded p-[20px]">
+            <div class="min-w-[max(250px,calc(100vw-100px))] sm:min-w-[300px] bg-white rounded p-[20px]">
                     
                 <div class="mx-auto block cursor-pointer text-white w-fit p-[5px] transition duration-300 bg-green-500 hover:bg-green-700" hx-get="{{ route('ajax.column.create', ['board_id' => $board->id]) }}" hx-target="#js-dialog">
                     Criar Coluna
