7b661257a571b03848d8dd766564d3b3f07bfd07
Juliano Seus <julianoseusdev@outlook.com>
2022-11-27 14:52:12 -0300

    More work on Edit Profile

--- resources/views/app/edit-profile/content.blade.php
+++ resources/views/app/edit-profile/content.blade.php
@@ -9,10 +9,49 @@
 
             <div class="flex mt-8">
                 <div class="flex-1">
-                    TESTE
+                    <div class="lg:pr-[20px]">
+                        <div class="cursor-pointer aspect-square w-[100%] bg-red-500">
+                        </div>
+                    </div>
                 </div>
                 <div class="flex-1">
-                    TESTE
+                    <div class="lg:pl-[20px]">
+                        <form method="POST" action="#">
+                            <div class="mb-4">
+                                <label class="block text-gray-700 text-sm font-bold mb-2" for="client_name">Nome</label>
+                                <div>
+                                    @error('client_name')
+                                        <span class="block text-red-600 mb-4" role="alert">
+                                            {{ $message }}
+                                        </span>
+                                    @enderror
+                                    <input type="text" name="client_name" value="{{ $client->name }}" required autofocus
+                                    class="
+                                    shadow-bootstrap appearance-none border border-solid border-coolgray rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    >
+                                </div>
+                            </div>
+                            <div class="mb-4">
+                                <label class="block text-gray-700 text-sm font-bold mb-2" for="client_email">E-mail</label>
+                                <div>
+                                    @error('client_email')
+                                        <span class="block text-red-600 mb-4" role="alert">
+                                            {{ $message }}
+                                        </span>
+                                    @enderror
+                                    <input type="email" name="client_email" value="{{ $client->email }}" required autofocus
+                                    class="
+                                    shadow-bootstrap appearance-none border border-solid border-coolgray rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    >
+                                </div>
+                            </div>
+                            <div class="flex justify-center mb-4">
+                                <button type="submit" class="w-fit transition duration-300 bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                                    Salvar
+                                </button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         
